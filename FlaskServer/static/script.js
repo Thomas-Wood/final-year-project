4ecde15ea2acdd83fe736bc4b650f6b4a805c4da
Thomas Wood <45307181+Thomas-Wood@users.noreply.github.com>
2022-04-13 12:51:16 +0100

    feat: Graphs now update with live data once a second

--- FlaskServer/static/script.js
+++ FlaskServer/static/script.js
@@ -1,60 +1,43 @@
 // Source: https://developers.sensorup.com/tutorials/chart/
-function getObservations(url) {
-  $.getJSON(url, function(observations) {
-    var data = $.map(observations.value, function(observation) {
-        var timestamp = moment(observation.phenomenonTime).valueOf();
-        observationTuple = [[timestamp, parseFloat(observation.result)]]
-        return observationTuple;
-    });
-    data.sort(function(a, b) {
-      return a[0] - b[0];
-    });
+function formatData(observations) {
+  var data = $.map(observations.value, function(observation) {
+    var timestamp = moment(observation.phenomenonTime).valueOf();
+    return [[timestamp, parseFloat(observation.result)]];
   });
-}
-
-// Source: https://www.highcharts.com/blog/code-examples/dynamically-updated-data/
-
-function onLoad(observationUrl) {
-  console.log("Called on load for: " + observationUrl)
-
-  // Add method to update series based on code below
-
-  // set up the updating of the chart each second
-  // var series = this.series[0];
-  // setInterval(function () {
-  //     var x = (new Date()).getTime(), // current time
-  //         y = Math.round(Math.random() * 100);
-  //     series.addPoint([x, y], true, true);
-  // }, 1000);
+  data.sort(function(a, b) {
+    return a[0] - b[0];
+  });
+  return data
 }
 
 function createChartObject(chartID, observationUrl, name, unitSymbol, unitName) {
 
   $.getJSON(observationUrl, function(observations) {
-    var data = $.map(observations.value, function(observation) {
-        var timestamp = moment(observation.phenomenonTime).valueOf();
-        return [[timestamp, parseFloat(observation.result)]];
-    });
-    data.sort(function(a, b) {
-      return a[0] - b[0];
-    });
+    var data = formatData(observations)
 
     var chart = new Highcharts.StockChart(chartID, {
       title: { text: "Loading Chart Data..." },
       series: [],
       chart: {
         events: {
-          load: function() {
-            onLoad(observationUrl)
-          }
+          load: function () {
+            // 'this' contains the chart object but is unreachable in the next funciton's scope unless defined here
+            var series = this.series
+            setInterval(function () {
+                $.getJSON(observationUrl, function(observations) {
+                    var data = formatData(observations)
+                    series[0].setData(data, redraw=true)
+                })
+            }, 5000);
+        }
         }
       }
     });
 
     chart.showLoading();
-
+    
     chart.setTitle({ text: name });
-
+    
     var series = chart.addSeries({
       data: data,
       tooltip: {
